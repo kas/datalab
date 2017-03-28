6c56d3f1e97d2868b42af1e90fd05e2201ba6f9c
Kenneth Schnall <kas6570@psu.edu>
2017-03-27 22:11:17 -0400

    Add comments

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ imgs = [
     'img/tigers/1.jpg',
     'img/tigers/2.jpg',
 ]
-shuffle(imgs)
+shuffle(imgs) # randomize imgs list
 
 def normalize(imgs):
-    '''Normalize BRG values in images to be between -1 and 1'''
+    '''Normalize BRG values in images from [0, 255] to [-1, 1]'''
     for img in imgs:
         im = cv2.imread(img)
         
-        im = im / 112.5
-        im -= 1
+        im = im / 112.5 # [0, 2]
+        im -= 1 # [-1, 1]
 
         yield (im, ''.join(imgs[0].split('/')[-2:]))
 
